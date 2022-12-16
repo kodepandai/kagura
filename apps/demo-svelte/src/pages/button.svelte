2c76d6339779f3936a3eabe588cb8914fc4bb54f
Akhmad Salafudin <axmad386@gmail.com>
2022-12-16 09:35:39 +0700

    feat(demo-svelte): change button size via input range

--- apps/demo-svelte/src/pages/button.svelte
+++ apps/demo-svelte/src/pages/button.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
   import { Button } from "@kagura-ui/svelte";
   import type { ButtonVariant } from "kagura-ui/contracts/button";
-  import type { RequiredColors, Size } from "kagura-ui/contracts/tailwind";
+  import type { RequiredColors } from "kagura-ui/contracts/tailwind";
   import { sizes } from "kagura-ui/utils";
   let preset = "mantine";
   let variant: ButtonVariant = "filled";
-  let size: Size = "sm";
+  let sizeRange: number = 2;
+  $: size = sizes[sizeRange - 1];
   type Color = keyof RequiredColors | "custom";
   let color: Color = "primary";
   const colors: Color[] = [
@@ -21,41 +22,39 @@
 </script>
 
 <div class="container mx-auto p-4 flex flex-col md:flex-row {preset}">
-  <div class="flex justify-center items-center min-h">
+  <div class="flex justify-center items-center w-2/3 border rounded">
     <Button {variant} {color} {size} class="capitalize">{color}</Button>
   </div>
-  <div class="mb-4">
-    <label for="theme">Preset</label>
+  <div class="flex flex-col border rounded flex-grow p-4">
+    <div class="mb-4">
+      <label for="theme">Preset</label>
 
-    <select bind:value={preset}>
-      <option value="bootstrap">Bootstrap</option>
-      <option value="mantine">Mantine</option>
-    </select>
-  </div>
-  <div class="mb-4">
-    <label for="theme">Color</label>
+      <select bind:value={preset}>
+        <option value="bootstrap">Bootstrap</option>
+        <option value="mantine">Mantine</option>
+      </select>
+    </div>
+    <div class="mb-4">
+      <label for="theme">Color</label>
 
-    <select bind:value={color}>
-      {#each colors as color}
-        <option value={color}>{color}</option>
-      {/each}
-    </select>
-  </div>
-  <div class="mb-4">
-    <label for="theme">Variant</label>
+      <select bind:value={color}>
+        {#each colors as color}
+          <option value={color}>{color}</option>
+        {/each}
+      </select>
+    </div>
+    <div class="mb-4">
+      <label for="theme">Variant</label>
 
-    <select bind:value={variant}>
-      <option value="filled">Filled</option>
-      <option value="outline">Outline</option>
-    </select>
-  </div>
-  <div class="mb-4">
-    <label for="theme">Size</label>
-
-    <select bind:value={size}>
-      {#each sizes as size}
-        <option value={size}>{size}</option>
-      {/each}
-    </select>
+      <select bind:value={variant}>
+        <option value="filled">Filled</option>
+        <option value="outline">Outline</option>
+      </select>
+    </div>
+    <div class="mb-4">
+      <label for="theme">Size</label>
+      <input type="range" min="1" max="5" bind:value={sizeRange} />
+      <span>{size}</span>
+    </div>
   </div>
 </div>
