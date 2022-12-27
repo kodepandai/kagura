5c20c378b9dd7d3e3f956dc1e309e5f7c47e9585
Akhmad Salafudin <axmad386@gmail.com>
2022-12-27 15:26:05 +0700

    feat(storybook): added Button story

---
+++ packages/kagura-ui-svelte/.storybook/main.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		'@storybook/addon-svelte-csf'
+	],
+	framework: {
+		name: '@storybook/sveltekit',
+		options: {}
+	},
+	docs: {
+		autodocs: 'tag'
+	}
+};
