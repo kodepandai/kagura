34a2fce1d62a1c0d6a1a84cc2bd17d14a370a066
Akhmad Salafudin <axmad386@gmail.com>
2023-01-29 01:28:04 +0700

    feat(preset-bootstrap): added spacing base

--- packages/kagura-ui-preset-bootstrap/src/index.ts
+++ packages/kagura-ui-preset-bootstrap/src/index.ts
@@ -1,7 +1,8 @@
 import { PresetCreator } from "kagura-ui/contracts/tailwind";
 import { createPreset } from "kagura-ui/utils";
 import { button } from "./components/button.js";
-import { colors } from "./components/color.js";
+import { colors } from "./base/color.js";
+import { spacing } from "./base/spacing.js";
 import { input } from "./components/input.js";
 import { inputWrapper } from "./components/inputWrapper.js";
 import { select } from "./components/select.js";
@@ -9,6 +10,7 @@ import { textArea } from "./components/textArea.js";
 
 const bootstrapPreset = {
   colors,
+  spacing,
   components: {
     button,
     input,
