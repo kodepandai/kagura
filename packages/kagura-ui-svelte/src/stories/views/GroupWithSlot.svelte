5c6f6d63f5aca4c1696040fc179a1b5b57018670
Akhmad Salafudin <axmad386@gmail.com>
2023-01-29 01:25:53 +0700

    feat(svelte): added Group component

---
+++ packages/kagura-ui-svelte/src/stories/views/GroupWithSlot.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Button from '../../lib/Button.svelte';
+	import Group from '../../lib/Group.svelte';
+</script>
+
+<Group class="bg-gray-100" {...$$restProps}>
+	<Button>1</Button>
+	<Button>2</Button>
+	<Button>3</Button>
+</Group>
