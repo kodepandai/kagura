9e214aae3b741b609861ee71f4d191a2cbe895da
Akhmad Salafudin <axmad386@gmail.com>
2023-01-29 01:27:33 +0700

    feat(preset-mantine): added spacing base

--- packages/kagura-ui-preset-mantine/src/index.ts
+++ packages/kagura-ui-preset-mantine/src/index.ts
@@ -1,7 +1,8 @@
 import { Preset, PresetCreator } from "kagura-ui/contracts/tailwind";
 import { createPreset } from "kagura-ui/utils";
+import { colors } from "./base/color.js";
+import { spacing } from "./base/spacing.js";
 import { button } from "./components/button.js";
-import { colors } from "./components/color.js";
 import { input } from "./components/input.js";
 import { inputWrapper } from "./components/inputWrapper.js";
 import { select } from "./components/select.js";
@@ -9,6 +10,7 @@ import { textArea } from "./components/textArea.js";
 
 const mantinePreset: Partial<Preset> = {
   colors,
+  spacing,
   components: {
     button,
     input,
